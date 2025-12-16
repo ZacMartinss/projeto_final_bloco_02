85a5e79129405c5be4af132b652b82c67481a494
Isaac Martins <isaacmartins431@gmail.com>
2025-12-16 15:03:41 -0300

    Correções finais no CRUD de produto

--- src/produto/controller/produto.controller.ts
+++ src/produto/controller/produto.controller.ts
@@ -6,38 +6,44 @@ import {
   Param,
   Post,
   Put,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { ProdutoService } from '../service/produto.service';
 import { Produto } from '../entity/produto.entity';
 
 @Controller('/produtos')
 export class ProdutoController {
-
   constructor(
     private readonly produtoService: ProdutoService,
   ) {}
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll(): Promise<Produto[]> {
     return this.produtoService.findAll();
   }
 
   @Get('/:id')
+  @HttpCode(HttpStatus.OK)
   findById(@Param('id') id: number): Promise<Produto | null> {
     return this.produtoService.findById(id);
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() produto: Produto): Promise<Produto> {
     return this.produtoService.save(produto);
   }
 
   @Put()
+  @HttpCode(HttpStatus.OK)
   update(@Body() produto: Produto): Promise<Produto> {
     return this.produtoService.update(produto);
   }
 
   @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number): Promise<void> {
     return this.produtoService.delete(id);
   }
